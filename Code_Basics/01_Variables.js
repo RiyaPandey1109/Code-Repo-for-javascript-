351c5689b72f9888e486625a814b0b6fba64ba74
Riya Pandey <riyapandey221203@gmail.com>
2024-06-19 21:17:40 +0530

    Starting with Javascript

---
+++ Code_Basics/01_Variables.js
@@ -0,0 +1,22 @@
+const  accountId = 144453
+let accountEmail= "riyapandey@gmail.com" // provides 
+var accountPassword="12345"
+accoutCity = "Jaipur"
+let accountState;
+
+
+// accountId=2 // not allowed
+accountEmail="ABC@gmaail.com"
+accountPassword="21234"
+accoutcity="Bengaluru"
+
+/* 
+Prefer not to use var
+because it creates issue in block scope and functional scope  
+ */
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accoutCity,accountState])
+
+
+
---
+++ Code_Basics/03_Conversions&Operations.js
@@ -0,0 +1,40 @@
+let score = "33A"
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc"=>NaN
+// true=> 1; false =>0;
+
+let isLoggedIn=1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber =33 
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// ***************** operations ****************8
+
+
+let value =3
+let negValue = -value
+
+console.log(negValue);
+
+//String operations
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
